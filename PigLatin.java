67fb8c2db8759c40268b59438e8e9cbe0510e9f4
Reshmi-Anwar <ranwar10@stuy.edu>
2021-01-02 18:46:00 -0500

    tested code

--- PigLatin.java
+++ PigLatin.java
@@ -10,6 +10,24 @@ public class PigLatin{
   public static String pigLatin(String s){
     s = s.toLowerCase();
     String[] listOfDigraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    if (s.length() <= 2){
+      return pigLatinSimple(s);
+    }
+    String twoCheck = s.substring(0, 2);
+    for(int i = 0; i < listOfDigraphs.length; i++){
+      if(twoCheck.equals(listOfDigraphs[i])){
+        return(s.substring(2) + twoCheck + "ay");
+      }
+    }
+    return pigLatinSimple(s);
+  }
+
+  public static void main(String[] args){
+    System.out.println(pigLatin("the"));
+    System.out.println(pigLatin("check"));
+    System.out.println(pigLatin("skee"));
+    System.out.println(pigLatin("emu"));
+    System.out.println(pigLatin("grade"));
   }
 
 }
