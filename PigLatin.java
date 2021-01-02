575595341bc0dc30871b79bbf0f6384857b32617
Reshmi-Anwar <ranwar10@stuy.edu>
2021-01-01 23:56:09 -0500

    aeiou first letter cases

--- PigLatin.java
+++ PigLatin.java
@@ -2,5 +2,8 @@ import java.util.*;
 public class PigLatin{
   public static String pigLatinSimple(String s){
     s = s.toLowerCase();
+    if ((s.charAt(0) == 'a') || (s.charAt(0) == 'e') || (s.charAt(0) == 'i') || (s.charAt(0) == 'o') || (s.charAt(0) == 'u')){
+      return (s + "hay");
+    }
   }
 }
