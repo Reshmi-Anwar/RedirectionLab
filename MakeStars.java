3ab0d801e777368ea164f47b3f907759197e4fa7
Reshmi-Anwar <ranwar10@stuy.edu>
2021-01-01 23:49:59 -0500

    tested code

--- MakeStars.java
+++ MakeStars.java
@@ -4,6 +4,18 @@ public class MakeStars{
     Scanner lineParse = new Scanner(System.in);
     while(lineParse.hasNextLine()){
       Scanner wordParse = new Scanner(lineParse.nextLine());
+      while(wordParse.hasNext()){
+        String wordStar = wordParse.next();
+        for(int i = 0; i < wordStar.length(); i++){
+          System.out.print("*");
+        }
+        if(wordParse.hasNext()){
+          System.out.print(" ");
+        }
+      }
+      if(lineParse.hasNextLine()){
+        System.out.println();
+      }
     }
   }
 }
