8a90203754b51418907697f1b64f4070b3da2463
Reshmi-Anwar <ranwar10@stuy.edu>
2021-01-01 23:44:46 -0500

    created scanner to parse through lines and then words

--- MakeStars.java
+++ MakeStars.java
@@ -1,6 +1,9 @@
 import java.util.*;
 public class MakeStars{
   public static void main(String[] args){
-
+    Scanner lineParse = new Scanner(System.in);
+    while(lineParse.hasNextLine()){
+      Scanner wordParse = new Scanner(lineParse.nextLine());
+    }
   }
 }
