569e51a71ed95aa540c98ffc4a9e2c09e0e4feb6
Reshmi-Anwar <ranwar10@stuy.edu>
2021-01-01 23:57:21 -0500

    other cases not aeiou first

--- PigLatin.java
+++ PigLatin.java
@@ -5,5 +5,6 @@ public class PigLatin{
     if ((s.charAt(0) == 'a') || (s.charAt(0) == 'e') || (s.charAt(0) == 'i') || (s.charAt(0) == 'o') || (s.charAt(0) == 'u')){
       return (s + "hay");
     }
+    return (s.substring(1) + s.charAt(0) + "ay");
   }
 }
