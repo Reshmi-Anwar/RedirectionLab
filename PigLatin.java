971eaec6d0e919accd99a2d98409e27d00348f08
Reshmi-Anwar <ranwar10@stuy.edu>
2021-01-04 12:09:20 -0500

    part 3 + created main to test

--- PigLatin.java
+++ PigLatin.java
@@ -23,15 +23,41 @@ public class PigLatin{
   }
   public static String pigLatinBest(String s){
     s = s.toLowerCase();
+    if ((s.charAt(0) > 'z') || (s.charAt(0) < 'a')){
+      return s;
+    }
+    //if punctuation case
+    else if ((s.charAt(s.length() - 1) > 'z') || (s.charAt(s.length() - 1) < 'a')){
+      return (pigLatin(s.substring(0, s.length() -1)) + s.substring(s.length() -1));
+    }
+    else{
+      return pigLatin(s);
+    }
   }
-  /*
+
   public static void main(String[] args){
+    Scanner line = new Scanner(System.in);
+    while(line.hasNextLine()){
+      Scanner words = new Scanner(line.nextLine());
+      while(words.hasNext()){
+        System.out.print(pigLatinBest(words.next()));
+        if(words.hasNext()){
+          System.out.print(" ");
+        }
+      }
+      if(line.hasNextLine()){
+        System.out.println();
+      }
+    }
+    /*
     System.out.println(pigLatin("the"));
     System.out.println(pigLatin("check"));
     System.out.println(pigLatin("skee"));
     System.out.println(pigLatin("emu"));
     System.out.println(pigLatin("grade"));
+    */
   }
-  */
+
+
 
 }
