6644ce885a00a438bc7ef969c15ff050cc8e801a
Reshmi-Anwar <ranwar10@stuy.edu>
2021-01-04 12:12:21 -0500

    compared output to expected output

--- PigLatin.java
+++ PigLatin.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class PigLatin{
+public class PigLatin {
   public static String pigLatinSimple(String s){
     s = s.toLowerCase();
     if ((s.charAt(0) == 'a') || (s.charAt(0) == 'e') || (s.charAt(0) == 'i') || (s.charAt(0) == 'o') || (s.charAt(0) == 'u')){
